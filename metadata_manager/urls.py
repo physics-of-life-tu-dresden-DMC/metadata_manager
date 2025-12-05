2cf7e3d0e9499e15140dbc4b3cbb1cf2ffe2b71a
physics-of-life-tu-dresden-DMC <meet_kaushal.bhatt@tu-dresden.de>
2025-12-05 10:07:30 +0100

    Add files via upload
    add : changes

---
+++ metadata_manager/urls.py
@@ -0,0 +1,8 @@
+# metadata_manager/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('metadata.urls')),
+]
